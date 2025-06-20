fd64d2210a75c280363a3dff584c50a565c98ed7
Kisetsu <dharshik06@gmail.com>
2025-06-20 23:40:55 +0530

    Commit id: #13\nAdded:\n1) Updation of documents.\n2) Auto-initialization of database directory.\n3) Collections can be created in default database.\nFixed:\nVulnerablities in handling current database.

--- Scripts/C#/Master.cs
+++ Scripts/C#/Master.cs
@@ -1,21 +1,27 @@
 ﻿using Kisetsu.Utils;
-using System.Text;
 
 namespace MicroDB {
 
     public static class Master {
 
-        public static string currentDatabase = "db";
-        public static Dictionary<string, string> databases = Json.Load<string>(Path.Combine(Directory.GetCurrentDirectory(), "db",".database.meta"));
+        public static string CurrentDatabase { get; private set; } = Token._database;
+        private static string DatabaseDirectory => Path.Combine(Directory.GetCurrentDirectory(), "db");
+        public static Dictionary<string, string> Databases { get; private set; } = Json.Load<string>(Path.Combine(DatabaseDirectory, ".database.meta"));
+
 
         public static void Main(string[] args) {
+            if (!Directory.Exists(DatabaseDirectory)) {
+                Directory.CreateDirectory(DatabaseDirectory);
+                Database.Create(Token._database);
+            }
             while (true) {
-                string input = Terminal.Input($"{currentDatabase}> ");
+                string input = Terminal.Input($"{CurrentDatabase}> ");
                 Parser.Execute(input);
             }
 
         }
 
-        
+        public static void SetDatabase(string name) => CurrentDatabase = name;
+
     }
 }
--- Scripts/C#/Parser.cs
+++ Scripts/C#/Parser.cs
@@ -4,7 +4,7 @@ using System.Text.RegularExpressions;
 namespace MicroDB {
 
     public static class Parser {
-        public record Query(string Object, string Action, string? Argument);
+        public record Query(string Object, string Operation, string? Argument);
 
         public static void Execute(string input) {
             var query = Parse(input);
@@ -14,58 +14,49 @@ namespace MicroDB {
             }
 
             switch (query.Object) {
-                case Clause._database:
-                    ExecuteDatabaseCommand(query);
-                    break;
-                case Clause.collection:
-                    ExecuteCollectionCommand(query);
-                    break;
-                default:
-                    ExecuteDocumentCommand(query);
-                    break;
+                case Token._database:  ExecuteDatabaseCommand(query);   break;
+                case Token.collection: ExecuteCollectionCommand(query); break;
+                default:    ExecuteDocumentCommand(query);  break;
             }
         }
 
         private static Query? Parse(string input) {
-            var match = Regex.Match(input, pattern: @"^(?<object>\w+)\.(?<action>\w+)\((?<argument>.*)\)$");
+            var match = Regex.Match(input, pattern: @"^(?<object>\w+)\.(?<operation>\w+)\((?<argument>.*)\)$");
             if (!match.Success) return null;
 
             return new Query(
-                match.Groups[Clause.@object].Value,
-                match.Groups[Clause.action].Value,
-                string.IsNullOrWhiteSpace(match.Groups[Clause.argument].Value) ? null : match.Groups[Clause.argument].Value.Trim('"')
+                match.Groups[Token.@object].Value,
+                match.Groups[Token.operation].Value,
+                string.IsNullOrWhiteSpace(match.Groups[Token.argument].Value) ? null : match.Groups[Token.argument].Value.Trim('"')
             );
         }
 
         private static void ExecuteDatabaseCommand(Query query) {
-            switch (query.Action) {
-                case Clause.use:    Database.Use(query.Argument!); break;
-                case Clause.create: Database.Create(query.Argument!); break;
-                case Clause.drop:   Database.Drop(query.Argument!); break;
-                case Clause.list:   Database.List(); break;
-                default: Terminal.WriteLine("Invalid database action", ConsoleColor.Red); break;
+            switch (query.Operation) {
+                case Token.use:    Database.Use(query.Argument!);    break;
+                case Token.create: Database.Create(query.Argument!); break;
+                case Token.drop:   Database.Drop(query.Argument!);   break;
+                case Token.list:   Database.List();                  break;
+                default:    Terminal.WriteLine("Invalid database operation", ConsoleColor.Red); break;
             }
         }
 
         private static void ExecuteCollectionCommand(Query query) {
-            switch (query.Action) {
-                case Clause.create: Collection.Create(query.Argument!); break;
-                case Clause.drop:   Collection.Drop(query.Argument!); break;
-                case Clause.list:   Collection.List(); break;
-                default: ExecuteDocumentCommand(query); break;
+            switch (query.Operation) {
+                case Token.create: Collection.Create(query.Argument!); break;
+                case Token.drop:   Collection.Drop(query.Argument!);   break;
+                case Token.list:   Collection.List();                  break;
+                default:    Terminal.WriteLine("Invalid collection operation", ConsoleColor.Red); break;
             }
         }
         private static void ExecuteDocumentCommand(Query query) {
-            switch (query.Action) {
-                case Clause.insert: Document.Insert(query); break;
-                case Clause.remove: Document.Remove(query); break;
-                case Clause.update: Document.Update(query); break;
-                case Clause.print:  Document.Print(query); break;
-                default: Terminal.WriteLine("Invalid collection action", ConsoleColor.Red); break;
+            switch (query.Operation) {
+                case Token.insert: Document.Insert(query); break;
+                case Token.remove: Document.Remove(query); break;
+                case Token.update: Document.Update(query); break;
+                case Token.print:  Document.Print(query);  break;
+                default:    Terminal.WriteLine("Invalid document operation", ConsoleColor.Red); break;
             }
         }
-
-
-
     }
 }
