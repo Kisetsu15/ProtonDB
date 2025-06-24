5ec7e9aa6c79955fa6c3e6b56947dfe5d474b5bb
Kisetsu <dharshik06@gmail.com>
2025-06-24 07:43:22 +0530

    Commit id: #38 Update: Removed verifying storage engine.

--- ProtonDB.CLI/Scripts/C#/Master.cs
+++ ProtonDB.CLI/Scripts/C#/Master.cs
@@ -3,27 +3,30 @@ using System.Text;
 
 namespace ProtonDB.CLI {
 
-    enum Operation {
-        Skip,
-        End,
-        Noop,
-    }
 
     public static class Master {
+
         private const int MAX_LENGTH = 4096;
         private const int MAX_ARGUMENT = 1;
+
+        private enum Operation {
+            Skip,
+            End,
+            Noop,
+        }
+
         private static readonly Dictionary<string, Func<Operation>> CommandMap = new(StringComparer.OrdinalIgnoreCase) {
-            [Token._quit]    = () => { Terminal.WriteLine("Exiting ProtonDB..."); return Operation.End; },
-            [Token.quit]     = () => { Terminal.WriteLine("Exiting ProtonDB..."); return Operation.End; },
+            [Token._quit]    = () => { Commands.Quit(); return Operation.End; },
+            [Token.quit]     = () => { Commands.Quit(); return Operation.End; },
             [Token.clear]    = () => { Commands.Clear();   return Operation.Skip; },
             [Token._help]    = () => { Commands.Help();    return Operation.Skip; },
             [Token.help]     = () => { Commands.Help();    return Operation.Skip; },
             [Token._version] = () => { Commands.Version(); return Operation.Skip; },
             [Token.version]  = () => { Commands.Version(); return Operation.Skip; }
         };
+
         public static void Main(string[] args) {
             ProtonMeta.Initialize();
-            if (!StorageEngine.IsActive()) return;
             if (args.Length > MAX_ARGUMENT) {
                 Terminal.WriteLine("ProtonDB takes only one argument. Usage: ProtonDB <command>");
                 return;
@@ -37,7 +40,6 @@ namespace ProtonDB.CLI {
             }
 
             while (true) {
-                if (!StorageEngine.IsActive()) break;
                 string input = Terminal.Input($"{ProtonMeta.CurrentDatabase}> ");
                 var op = CommandExecutor(input);
                 if (op == Operation.End) break;
@@ -46,7 +48,6 @@ namespace ProtonDB.CLI {
                 Parser.Execute(input);
             }
         }
-
 
 
         private static Operation CommandExecutor(string input) {
