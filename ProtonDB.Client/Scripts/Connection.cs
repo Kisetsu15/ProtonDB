e31b90fdad04704b50a0202c0f4faa97a1988cb1
Kisetsu <dharshik06@gmail.com>
2025-06-27 23:59:44 +0530

    New Cursor created

--- ProtonDB.Client/Scripts/Connection.cs
+++ ProtonDB.Client/Scripts/Connection.cs
@@ -5,6 +5,7 @@ namespace ProtonDB.Client {
         private ProtonDBSession _session;
         public const string defaultHost = "127.0.0.1";
         public const int defaultPort = 9090;
+        public ProtonDBSession Session => _session;
 
         private string _host = defaultHost;
         private readonly int _port = defaultPort;
@@ -47,69 +48,9 @@ namespace ProtonDB.Client {
             }
         }
 
-
-        public void Query(string query) {
-            var response = _session.QueryAsync(query).GetAwaiter().GetResult();
-            if (response.Status != "ok") {
-                throw new Exception($"Query failed: {response.Message}");
-            }
-        }
-
-        public void SafeQuery(string query) {
-            try {
-                Query(query);
-            } catch (Exception) {
-                Reconnect();
-                Query(query);
-            }
-        }
-
-        public string QueryRaw(string query) {
-            var res = _session.QueryAsync(query).GetAwaiter().GetResult();
-            return res.Message;
-        }
-
-
-        public string[] FetchAll() {
-            var response = _session.FetchAsync().GetAwaiter().GetResult();
-            if (response.Result == null || response.Result.Length == 0) {
-                return [];
-            }
-            return response.Result;
-        }
-
-        public string FetchOne() {
-            var response = _session.FetchAsync().GetAwaiter().GetResult();
-            if (response.Result == null || response.Result.Length == 0) {
-                return string.Empty;
-            }
-            return response.Result[0];
-        }
-
-        public void Debug(bool enable) {
-            var response = _session.DebugAsync(enable).GetAwaiter().GetResult();
-            if (response.Status != "ok") {
-                throw new Exception($"Debug command failed: {response.Message}");
-            }
-        }
-
-        public string Quit() {
-            var response = _session.QuitAsync().GetAwaiter().GetResult();
-            if (response.Status != "ok") {
-                throw new Exception($"Quit command failed: {response.Message}");
-            } else {
-                _session.Dispose();
-                return response.Message;
-            }
-        }
-
-        public string Version() => $" ProtonDB v{Assembly.GetExecutingAssembly().GetName().Version}";
-
+        
         public void Dispose() {
             _session.Dispose();
         }
     }
-
-
-
 }
---
+++ ProtonDB.Client/Scripts/Cursor.cs
@@ -0,0 +1,64 @@
+﻿using System.Reflection;
+
+namespace ProtonDB.Client{
+    public class Cursor(Connection connection) {
+        private readonly ProtonDBSession _session = connection.Session;
+
+        public void Query(string query) {
+            var response = _session.QueryAsync(query).GetAwaiter().GetResult();
+            if (response.Status != "ok") {
+                throw new Exception($"Query failed: {response.Message}");
+            }
+        }
+
+        public void SafeQuery(string query) {
+            try {
+                Query(query);
+            } catch (Exception) {
+                connection.Reconnect();
+                Query(query);
+            }
+        }
+
+        public string QueryRaw(string query) {
+            var res = _session.QueryAsync(query).GetAwaiter().GetResult();
+            return res.Message;
+        }
+
+
+        public string[] FetchAll() {
+            var response = _session.FetchAsync().GetAwaiter().GetResult();
+            if (response.Result == null || response.Result.Length == 0) {
+                return [];
+            }
+            return response.Result;
+        }
+
+        public string FetchOne() {
+            var response = _session.FetchAsync().GetAwaiter().GetResult();
+            if (response.Result == null || response.Result.Length == 0) {
+                return string.Empty;
+            }
+            return response.Result[0];
+        }
+
+        public void Debug(bool enable) {
+            var response = _session.DebugAsync(enable).GetAwaiter().GetResult();
+            if (response.Status != "ok") {
+                throw new Exception($"Debug command failed: {response.Message}");
+            }
+        }
+
+        public string Quit() {
+            var response = _session.QuitAsync().GetAwaiter().GetResult();
+            if (response.Status != "ok") {
+                throw new Exception($"Quit command failed: {response.Message}");
+            } else {
+                _session.Dispose();
+                return response.Message;
+            }
+        }
+
+        public string Version() => $"ProtonDB v{Assembly.GetExecutingAssembly().GetName().Version}";
+    }
+}
