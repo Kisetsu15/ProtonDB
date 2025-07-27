a0a6abf832bc5e0a43dc2ecad8707a2e5816ea52
sarvesh <sarveshjrgar@gmail.com>
2025-07-28 00:19:34 +0530

    Update ProtonDBclient.py

--- Wrapper/pyproton/src/ProtonDBclient.py
+++ Wrapper/pyproton/src/ProtonDBclient.py
@@ -1,8 +1,8 @@
 from typing import Optional
-from connection import Connection
-from ProtonRequest import ProtonRequest
-from ProtonResponse import ProtonResponse
-from protondbdriver import ProtonDBDriver
+from .connection import Connection
+from .ProtonRequest import ProtonRequest
+from .ProtonResponse import ProtonResponse
+from .protondbdriver import ProtonDBDriver
 
 class ProtonDBClient:
     def __init__(self, host, port, username, password):
